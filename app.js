aa76bffbf02c8ebe3dadcce170ab3885f997b042
Colin Frogner <Colin Frogner>
2016-06-08 21:29:53 -0700

    Cleaned up the useless files. It's pretty much done now. Added media queries for I think most sizes of phones and tablets. Best experienced at fullscreen on Desktop.

--- app.js
+++ app.js
@@ -1,151 +1,130 @@
-// 1 = pause button, 0 = play button
-var pauseOrResume = 1;
+$('#shineOn').hide();
+$('#confetti').hide();
+$('.revealText').hide();
+$("body").css("overflow", "hidden");
 
-var currentVideoIndex = 0;
+var elem0 = document.getElementById("hat"); 
+var elem1 = document.getElementById("Thomas"); 
+var elem2 = document.getElementById("shineOn");
+var pos0 = -440;
+var pos1 = window.innerHeight + 10;
+var id = setInterval(frame, 10);
+var hatThomasConnect = 0;
+var parent = document.getElementById("mainBody");
 
-var videoGBLink = "PlaceHolder";
+myAudio = new Audio('testAudio.mp3'); 
+myAudio.addEventListener('ended', function() {
+	this.currentTime = 0;
+	this.play();
+}, false);
+//myAudio.play();
 
-var timeoutPlay = 0;
-var timeoutSeek = 0;
-var timeoutResume = 0;
-var timeoutFinish = 0;
-var dateTime = new Date().getTime() / 1000;
-var startTime = 1464738588;
-var globalTimeStamp = Math.floor(dateTime - startTime);
-var videoTimeStamp = 0;
-var totalLength = 0;
-
-console.log("Start Time: " + dateTime);
-console.log("Current Time: " + globalTimeStamp);
-console.log("Minutes: " + Math.floor(globalTimeStamp / 60));
-console.log("Seconds: " + Math.floor(globalTimeStamp % 60));
-
-// Serve up a video when page loads
-$(document).ready(calculateCurrentVideo());
-pickRandomQuickLook();
-setInterval(changeTime,1000);
-
-// Gets random video id and name from video array
-function pickRandomQuickLook(){
-	resetVideoTimeouts();
-	document.getElementById("currVideo").src='http://www.giantbomb.com/videos/embed/' + quickLooks[currentVideoIndex].split('~')[0] + '/';
-	document.getElementById("VideoTitle").innerHTML=quickLooks[currentVideoIndex].split('~')[1];
-	videoGBLink=quickLooks[currentVideoIndex].split('~')[3];
+function frame() {
+	hatThomasConnect = pos1 - pos0;
+	if (hatThomasConnect <= 85) {
+		clearInterval(id);
+		$("#Thomas").attr('src', 'img/happyFace.png');
+		var shineTop = parseInt($('#Thomas').css('top').replace(/[^\d.]/g, '' ));
+		var shineLeft = parseInt($('#Thomas').css('left').replace(/[^\d.]/g, '' ));
+		elem2.style.top= (shineTop - 1121) + 'px';
+		elem2.style.left = (shineLeft - 1375) + 'px';
+		rotateAnimation('shineOn', 40);
+		$('#shineOn').fadeIn("slow");
+		$('#confetti').fadeIn("slow");
+		
+		setTimeout(function(){$('.revealText').fadeIn("slow");}, 3000);
+		myAudio.play();
+		var spawnImages = setInterval(startImageSpawner, 1000);
+		var moveID = setInterval(moveImages, 10);
+	} else {
+		pos0++; 
+		pos1--;
+		
+		//pos0 += 10;
+		//pos1 -= 10;
+		elem0.style.top = pos0 + 'px'; 
+		elem1.style.top = pos1 + 'px'; 
+	}
+}
+function spin() {
+	var c = document.getElementById("Thomas");
+	var ctx = c.getContext("2d");
+	ctx.rotate(20 * Math.PI / 180);
+	ctx.fillRect(50, 20, 100, 50);
 }
 
-function changeTime(){
-	globalTimeStamp += 1;
-	var hours = Math.floor(globalTimeStamp / 3600);
-	var minutes = Math.floor((globalTimeStamp % 3600) / 60);
-	if(minutes < 10){
-		minutes = "0" + minutes;
+//Code below found at https://www.developphp.com/video/JavaScript/Transform-Rotate-Image-Spin-Smooth-Animation-Tutorial
+var looper;
+var degrees = 0;
+function rotateAnimation(el,speed){
+	var elem = document.getElementById(el);
+	if(navigator.userAgent.match("Chrome")){
+		elem.style.WebkitTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Firefox")){
+		elem.style.MozTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("MSIE")){
+		elem.style.msTransform = "rotate("+degrees+"deg)";
+	} else if(navigator.userAgent.match("Opera")){
+		elem.style.OTransform = "rotate("+degrees+"deg)";
+	} else {
+		elem.style.transform = "rotate("+degrees+"deg)";
 	}
-	var seconds = Math.floor((globalTimeStamp % 3600) % 60);
-	if(seconds < 10){
-		seconds = "0" + seconds;
+	looper = setTimeout('rotateAnimation(\''+el+'\','+speed+')',speed);
+	degrees++;
+	if(degrees > 359){
+		degrees = 1;
 	}
+}
+
+function startImageSpawner() {
+	var moveSpeed = (Math.random() * 5) + 1;
+	var leftSpawn = (Math.random() * (window.innerWidth - 100)) + 1;
+	var imageSize = (Math.random() * 300) + 150;
+	var imageNumber = Math.floor((Math.random() * 56));
 	
-	if(hours > 0){
-		$('#timeSinceStart').html("Current Universal Time Stamp - " + hours + ":" + minutes + ":" + seconds);
-	}else{
-		$('#timeSinceStart').html("Current Universal Time Stamp - " + minutes + ":" + seconds);
-	}
+	var x = document.createElement("IMG");
+	x.setAttribute("src", "img/TomGradPhotos/"+ imageNumber +".jpg");
+	x.setAttribute("class", "tempImage");
+	x.setAttribute("id", "moving");
+	x.setAttribute("width", imageSize);
+	x.setAttribute("alt", moveSpeed);
+	x.style.cssText = 'position:absolute;top:-500px;left:'+ leftSpawn +'px;';
+	document.body.appendChild(x);
 }
 
-function calculateCurrentVideo(){
+function deleteAllImages() {
 	var i = 0;
-	var lastLength = 0;
-	totalLength = 0;
+	var arrayStuff = document.getElementsByClassName("deleteImage");
 	
-	dateTime = new Date().getTime() / 1000;
-	globalTimeStamp = Math.floor(dateTime - startTime);
-	
-	console.log("Before Calc: " + globalTimeStamp);
-	
-	for(i = 0; totalLength < globalTimeStamp; i++){
-		totalLength = totalLength + parseInt(quickLooks[i].split('~')[2]);
-		console.log("Video Length: " + parseInt(quickLooks[i].split('~')[2]))
-		lastLength = parseInt(quickLooks[i].split('~')[2]);
-		console.log(totalLength + " Index: " + i);
+	while(arrayStuff.length){
+		parent.removeChild(arrayStuff[0]);
 	}
-	console.log(totalLength);
-	console.log(lastLength);
-	globalTimeStamp = lastLength - (totalLength - globalTimeStamp);
-	currentVideoIndex = i - 1;
-	console.log(currentVideoIndex);
-	console.log("After Calc: " + globalTimeStamp);
+	
 }
 
-function resetVideoTimeouts(){
-	clearTimeout(timeoutPlay);
-	clearTimeout(timeoutSeek);
-	clearTimeout(timeoutResume);
-	clearTimeout(timeoutFinish);
-}
-
-function openGBLink(){
-	var win = window.open(videoGBLink, '_blank');
-	if(win){
-		//Browser has allowed it to be opened
-		win.focus();
-	}else{
-		//Broswer has blocked it
-		alert('Please allow popups for this site');
-	}
-}
-
-// pause,resume button animation
-$("#resumePause").mousedown(playPressed);
-$("#resumePause").mouseup(playReleased);
-$("#resumePause").keydown(function (e) {
-		if(e.keyCode === 32){
-			playPressed();
+function moveImages() {
+	
+	var i = 0;
+	var arrayStuff = document.getElementsByClassName("tempImage");
+	
+	for(i = 0; i < arrayStuff.length; i++){
+		if(arrayStuff[i].id == "moving"){
+			var newPos = parseInt($(arrayStuff[i]).css('top').split('p')[0]);
+			newPos += parseInt($(arrayStuff[i]).attr("alt"));
+			$(arrayStuff[i]).css('top', newPos + 'px');
+			if(newPos > (window.innerHeight)){
+				$(arrayStuff[i]).attr("class","deleteImage");
+			}
 		}
-});
-$("#resumePause").keyup(function (e) {
-		if(e.keyCode === 32){
-			playReleased();
-		}
-});
-
-
-// pause,resume button animation
-function playReleased(){
-	if(pauseOrResume == 0){
-		$("#resumePause").attr("src", "play_0.png");
-		$("#resumePause").attr("onClick", "pauseVideo();");
-	}else{
-		$("#resumePause").attr("src", "Buttons_0.png");
-		$("#resumePause").attr("onClick", "resumeVideo();");
+		deleteAllImages();
 	}
 }
-
-// pause,resume button animation
-function playPressed(){
-	if(pauseOrResume == 0){
-		pauseOrResume = 1;
-		$("#resumePause").attr("src", "play_1.png");
-	}else{
-		pauseOrResume = 0;
-		$("#resumePause").attr("src", "Buttons_3.png");
-	}
-}
-
-// Next video button animation
-$("#change").mousedown(function(){
-		$("#change").attr("src", "ChangeBDown.png");
-});
-
-// Next video button animation
-$("#change").mouseup(function(){
-		$("#change").attr("src", "ChangeBUp.png");
-});
-
-$("#GBLink").mousedown(function(){
-		$("#GBLink").attr("src", "GButton1.png");
-});
-
-// Next video button animation
-$("#GBLink").mouseup(function(){
-		$("#GBLink").attr("src", "GButton0.png");
-});
+		
+		
+		
+		
+		
+		
+		
+		
+		
